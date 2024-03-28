f494836bc18ecdd71d7c38a1fbf4c9697b4cd02f
Nikhil172913832 <nikhilarora13832@gmail.com>
2024-03-29 00:02:39 +0530

    dhruv choder

--- index5.js
+++ index5.js
@@ -1,23 +1,26 @@
-
 function redirectTo(url) {
     window.location.href = url;
 }
+
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('.btn2').onclick = function () {
-        redirectTo('sign.html'); } ; 
-        document.querySelector('.btn1').onclick = function () {
-            redirectTo('2ndpage.html'); 
-   
-        } 
-}); 
-/*function valid() {
-var email = document.querySelector("#email").value ; 
-var pswd = document.querySelector("password").value ; 
-if( email === "23122@iiitu.ac.in" && pswd==="12345"){
+        redirectTo('sign.html');
+    };
+
+    document.querySelector('.btn1').onclick = function () {
+        valid(); // Call the valid function
+    };
+});
+
+function valid() {
+    var email = document.querySelector("#email1").value;
+    var pswd = document.querySelector("#password1").value; // Correct selector for password field
+    redirectTo('2ndpage.html');
+    alert("Email: " + email + "\nPassword: " + pswd);
+    if( email === "23122@iiitu.ac.in" && pswd==="12345"){
   
         document.querySelector('.btn1').onclick = function () {
             redirectTo('2ndpage.html'); 
  }  
 } } 
-document.querySelector('.btn2').onclick = function () {
-    redirectTo('sign.html'); } ; */
+
